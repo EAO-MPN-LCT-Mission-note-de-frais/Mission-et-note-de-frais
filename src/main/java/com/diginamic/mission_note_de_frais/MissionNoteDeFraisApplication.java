334e9c62bb5d57ba91e3737d34f21e157b5f2067
ericafenyo <ericafenyo@yahoo.com>
2024-11-19 12:13:02 +0100

    initial commit

---
+++ src/main/java/com/diginamic/mission_note_de_frais/MissionNoteDeFraisApplication.java
@@ -0,0 +1,13 @@
+package com.diginamic.mission_note_de_frais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionNoteDeFraisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionNoteDeFraisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diginamic/mission_note_de_frais/MissionNoteDeFraisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diginamic.mission_note_de_frais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionNoteDeFraisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
