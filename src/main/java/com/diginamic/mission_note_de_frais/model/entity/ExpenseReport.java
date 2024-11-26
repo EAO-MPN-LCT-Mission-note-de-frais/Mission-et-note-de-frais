067c4f15ee588373cc1c9c3bf89e6872ea2384c2
ericafenyo <ericafenyo@yahoo.com>
2024-11-26 13:53:50 +0100

    N/A|link status and mission to an expense type

--- src/main/java/com/diginamic/mission_note_de_frais/model/entity/ExpenseReport.java
+++ src/main/java/com/diginamic/mission_note_de_frais/model/entity/ExpenseReport.java
@@ -1,38 +1,55 @@
 package com.diginamic.mission_note_de_frais.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Modèle représentant une note de frais contenant des lignes de frais.
- *
+ * <p>
  * TODO: Implémenter la mission rattachée et le statut de la note de frais
  *
  * @author Marjory PRIN
  */
-@Entity
-@Table(name = "EXPENSE_REPORT")
+@Entity(name = "EXPENSE_REPORT")
 @Data
 public class ExpenseReport {
 
-    /**
-     * Identifiant unique de la note de frais
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  /**
+   * Identifiant unique de la note de frais
+   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    /**
-     * Liste des lignes de frais associées
-     */
-    @NonNull
-    @OneToMany(mappedBy = "expenseReport")
-    private List<Expense> expenses = new ArrayList<>();
+  /**
+   * Liste des lignes de frais associées
+   */
+  @NonNull
+  @OneToMany(mappedBy = "expenseReport")
+  private List<Expense> expenses = new ArrayList<>();
 
+  /**
+   * Mission rattachée à la note de frais
+   */
+  @OneToOne()
+  @JoinColumn(name = "mission_id")
+  private Mission mission;
+
+  /**
+   * Statut de la note de frais
+   */
+  @ManyToOne
+  @JoinColumn(name = "status_id")
+  Status status;
 }
