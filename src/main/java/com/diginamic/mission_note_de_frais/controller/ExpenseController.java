52f6ee901970b668b0854a01306103778945c477
Marjory PRIN <marjory.prin@gmail.com>
2024-11-26 14:53:29 +0100

    #23 | Ajout d"une ligne de frais en base

--- src/main/java/com/diginamic/mission_note_de_frais/controller/ExpenseController.java
+++ src/main/java/com/diginamic/mission_note_de_frais/controller/ExpenseController.java
@@ -1,11 +1,15 @@
 package com.diginamic.mission_note_de_frais.controller;
 
+import com.diginamic.mission_note_de_frais.exception.FunctionalException;
 import com.diginamic.mission_note_de_frais.model.dto.ExpenseDTO;
+import com.diginamic.mission_note_de_frais.model.entity.Expense;
 import com.diginamic.mission_note_de_frais.model.entity.ExpenseReport;
 import com.diginamic.mission_note_de_frais.model.mapper.ExpenseMapper;
 import com.diginamic.mission_note_de_frais.service.ExpenseReportServiceImpl;
 import com.diginamic.mission_note_de_frais.service.ExpenseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -62,9 +66,29 @@ public class ExpenseController {
     /**
      * Insère une nouvelle ligne de frais.
      *
-     * @param expenseDTO Le DTO de Expense à insérer.
-     * @return ResponseEntity avec le statut HTTP et l'entité insérée.
+     * @param newExpense Le DTO de Expense à insérer.
+     * @param expenseReportId L'identifiant de la note de frais auquel cette dépense est associée.
+     * @return ResponseEntity avec le statut HTTP et un message.
      */
+    @PostMapping
+    public ResponseEntity<String> insertExpense(@RequestBody ExpenseDTO newExpense, @RequestParam Long expenseReportId) throws FunctionalException {
+        try {
+            ExpenseReport expenseReport = expenseReportService.getExpenseReportById(expenseReportId);
+            Expense expense = expenseMapper.toEntity(newExpense);
+            expense.setExpenseReport(expenseReport);
 
+            boolean result = expenseService.insertExpense(expense);
+
+            if (result) {
+                return new ResponseEntity<String>("Dépense insérée avec succès", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Echec de l'insertion : La dépense n'a pas pu être insérée pour une raison inconnue", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (FunctionalException e) {
+            return new ResponseEntity<>("Erreur de validation (400) : " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erreur interne du serveur : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
