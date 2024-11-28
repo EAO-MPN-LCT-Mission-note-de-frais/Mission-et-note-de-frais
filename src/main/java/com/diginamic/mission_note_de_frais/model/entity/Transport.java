5b4ab48b96e4077e580ead001198a81835658e28
ericafenyo <ericafenyo@yahoo.com>
2024-11-28 09:50:29 +0100

    #13|remove star imports. It increases import size

--- src/main/java/com/diginamic/mission_note_de_frais/model/entity/Transport.java
+++ src/main/java/com/diginamic/mission_note_de_frais/model/entity/Transport.java
@@ -1,6 +1,12 @@
 package com.diginamic.mission_note_de_frais.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
 import java.util.HashSet;
 import java.util.Set;
 
